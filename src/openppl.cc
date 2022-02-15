fa98c101f469564d2ef35b14092eb7c1f1d6dc5d
xusi <xusi@sensetime.com>
2022-02-15 10:52:08 +0800

    215 test

--- src/openppl.cc
+++ src/openppl.cc
@@ -155,7 +155,7 @@ class ModelInstanceState : public BackendModelInstance {
       ModelInstanceState** state);
   virtual ~ModelInstanceState() {
       LOG(ERROR) << "Begin to deconstruct ModelInstanceState";
-      delete runtime_.get();
+      // delete runtime_.get();
       // delete builder_.get();
       // for (uint32_t i = 0; i < engines_.size(); ++i) {
       //     delete engines_[i].get();
